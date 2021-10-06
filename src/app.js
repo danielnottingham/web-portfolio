7aeeda63f492919e731267594627716265666c63
Daniel Nottingham <danielnottingham.ti@gmail.com>
2021-10-06 16:55:15 -0300

    Custom Scrollbar and better transition for nav links

--- src/app.js
+++ src/app.js
@@ -2,15 +2,14 @@
 function typeWritter(element) {
   const textArray = element.innerHTML.split('');
   element.innerHTML = ""
- 
+
   textArray.forEach((letter, i) => {
     setTimeout(()=> element.innerHTML += letter, 100 * i);
   });
 }
- 
- 
+
 const typingPhrase = document.getElementById('typing-phrase')
- 
+
 setInterval( function() {
   typeWritter(typingPhrase)
 }, 3000 );
@@ -20,4 +19,13 @@ const switchElement = document.querySelector('.switch')
 
 switchElement.addEventListener('click', () => {
   document.body.classList.toggle('dark')
+})
+
+/*  CUSTOM SCROLL BAR  */
+$(window).scroll(function() {
+  var scroll  = $(window).scrollTop(),
+  dh = $(document).height(),
+  wh = $(window).height();
+  scrollPercent = (scroll / (dh-wh)) * 100;
+  $('#progressBar').css('height', scrollPercent + '%');
 })
